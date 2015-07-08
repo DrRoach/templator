07d862058a7086db9f3a5d8158199e407e77a088
DrRoach <ryandeas1@gmail.com>
2015-07-08 03:19:14 +0100

    Fixed bug with empty val checking

--- regex/FindIncrement.php
+++ regex/FindIncrement.php
@@ -5,7 +5,7 @@ class FindIncrement
     public static function run($line)
     {
         if (preg_match_all('/(?<start>.*){{\s*(?<beforeInc>\+\+|--)?(?<variable>\w+)(?<afterInc>\+\+|--)?\s*}}(?<end>.*)/', $line, $match)) {
-            if (!empty($match['beforeInc']) || !empty($match['afterInc'])) {
+            if (!empty($match['beforeInc'][0]) || !empty($match['afterInc'][0])) {
                 return Parse::run($match);
             } else {
                 return false;
