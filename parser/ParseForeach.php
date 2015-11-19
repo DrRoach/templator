bbb5019b3034d37493c72f7ee7475b1731a0a440
Ryan Deas <ryandeas1@gmail.com>
2015-11-19 19:12:19 +0000

    Added ability to loop through a nested array

--- parser/ParseForeach.php
+++ parser/ParseForeach.php
@@ -4,7 +4,21 @@ class ParseForeach
 {
     public static function run($line)
     {
-        $echo = '<?php foreach($' . $line['array'] . ' as $' . $line['var'] . (!empty($line['subVar']) ? ' => $'.$line['subVar'] : '') .') : ?>';
+        $split = explode('.', $line['array']);
+        $array = '';
+
+        foreach ($split as $pos => $var) {
+            if ($pos == 0) {
+                $array .= '$'.$var;
+            } else {
+                $array .= $var . "']";
+            }
+            if ((sizeof($split) - 1) > $pos && sizeof($split) > 1) {
+                $array .= "['";
+            }
+        }
+
+        $echo = '<?php foreach(' . $array . ' as $' . $line['var'] . (!empty($line['subVar']) ? ' => $'.$line['subVar'] : '') .') : ?>';
 
         return $line['start'] . $echo . $line['end'];
     }
--- regex/FindForeach.php
+++ regex/FindForeach.php
@@ -4,7 +4,7 @@ class FindForeach
 {
     public static function run($line)
     {
-        if(preg_match_all('/(?<start>.*){{\s*foreach\s*\((?<array>\w+)\s*as\s*(?<var>\w+)(\s*=>\s*(?<subVar>\w+))?\s*\)\s*}}(?<end>.*)/', $line, $matches)) {
+        if(preg_match_all('/(?<start>.*){{\s*foreach\s*\((?<array>[\w|\.]+)\s*as\s*(?<var>\w+)(\s*=>\s*(?<subVar>\w+))?\s*\)\s*}}(?<end>.*)/', $line, $matches)) {
             return Parse::run($matches);
         }
         return false;
